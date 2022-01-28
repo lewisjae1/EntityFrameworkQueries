c4f2d507f1277d1c39d9d5df5904f00685dc11a8
Jaein Lewis <dlwodls0115@gmail.com>
2022-01-27 18:51:49 -0800

    Added example to select all vendors

--- EntityFrameworkQuerires/Form1.Designer.cs
+++ EntityFrameworkQuerires/Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSelectAllVendors = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelectAllVendors
+            // 
+            this.btnSelectAllVendors.Location = new System.Drawing.Point(32, 30);
+            this.btnSelectAllVendors.Name = "btnSelectAllVendors";
+            this.btnSelectAllVendors.Size = new System.Drawing.Size(230, 29);
+            this.btnSelectAllVendors.TabIndex = 0;
+            this.btnSelectAllVendors.Text = "SELECT * FROM Vendors";
+            this.btnSelectAllVendors.UseVisualStyleBackColor = true;
+            this.btnSelectAllVendors.Click += new System.EventHandler(this.btnSelectAllVendors_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(32, 198);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnSelectAllVendors);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button btnSelectAllVendors;
+        private Button button2;
+        private Button button3;
     }
 }
--- EntityFrameworkQuerires/Form1.cs
+++ EntityFrameworkQuerires/Form1.cs
@@ -6,5 +6,17 @@ namespace EntityFrameworkQuerires
         {
             InitializeComponent();
         }
+
+        private void btnSelectAllVendors_Click(object sender, EventArgs e)
+        {
+            using APContext dbContext = new();
+
+            // LINQ(Language Integrated Query) method syntax
+            List<Vendor> vendorList = dbContext.Vendors.ToList();
+
+            // LINQ query syntax
+            List<Vendor> vendorList2 = (from v in dbContext.Vendors
+                                       select v).ToList();
+        }
     }
 }
